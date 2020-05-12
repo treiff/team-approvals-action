084389456e32c67dc9998a22242d73a82d62923d
Trevor Reiff <trevorreiff@gmail.com>
2020-05-12 09:35:18 -0400

    Working demo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,23 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run(): Promise<void> {
+  try {
+    const ms: string = core.getInput('milliseconds')
+    const nameToGreet: string = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+
+    core.debug(`Waiting ${ms} milliseconds ...`)
+
+    core.debug(new Date().toTimeString())
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    core.setOutput('time', new Date().toTimeString())
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
